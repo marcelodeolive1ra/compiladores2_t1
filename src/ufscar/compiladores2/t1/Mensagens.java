516bff1123cd38163f1578d0d9797e7cba346f25
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2016-11-30 16:52:49 -0200

    Remove código desnecessário na classe Mensagens

--- src/ufscar/compiladores2/t1/Mensagens.java
+++ src/ufscar/compiladores2/t1/Mensagens.java
@@ -1,50 +1,35 @@
 package ufscar.compiladores2.t1;
 
-/**
- * Created by marcelodeoliveiradasilva on 19/11/16.
- */
+// Classe que adiciona ao arquivo de saída mensagens de erros semânticos
+
 public class Mensagens {
+    private static String mensagens_de_erro = "";
 
-    // Classe que adiciona ao arquivo de saída mensagens de erro semântico
-
-    private static StringBuffer text = new StringBuffer();
-    private static boolean modificado = false;
-
-    public static void erroVariavelJaExiste(String var, int line) {
-        if(!modificado) modificado = true;
-        text.append("Linha "+line +": identificador "+var+" ja declarado anteriormente").append('\n');
+    public static void erroVariavelJaExiste(String variavel, int linha) {
+        mensagens_de_erro += "Linha " + linha + ": identificador " + variavel + " ja declarado anteriormente\n";
     }
 
-    public static void erroVariavelNaoExiste(String var, int line) {
-        if(!modificado) modificado = true;
-        text.append("Linha "+line +": identificador "+var+" nao declarado").append('\n');
+    public static void erroVariavelNaoExiste(String variavel, int linha) {
+        mensagens_de_erro += "Linha " + linha + ": identificador " + variavel + " nao declarado\n";
     }
 
-    public static void escopoNaoPermitido(int line) {
-        if(!modificado) modificado = true;
-        text.append("Linha "+ line +": comando retorne nao permitido nesse escopo").append("\n");
+    public static void escopoNaoPermitido(int linha) {
+        mensagens_de_erro += "Linha " + linha + ": comando retorne nao permitido nesse escopo\n";
     }
 
-    public static void erroTipoNaoExiste(String tipo, int line) {
-        if(!modificado) modificado = true;
-        text.append("Linha "+ line +": tipo "+tipo+ " nao declarado").append("\n");
+    public static void erroTipoNaoExiste(String tipo, int linha) {
+        mensagens_de_erro += "Linha " + linha + ": tipo " + tipo + " nao declarado\n";
     }
 
-    public static void erroVariavelNaoCompativel(String var, int line) {
-        if(!modificado) modificado = true;
-        text.append("Linha "+line +": atribuicao nao compativel para "+var).append('\n');
+    public static void erroVariavelNaoCompativel(String variavel, int linha) {
+        mensagens_de_erro += "Linha " + linha + ": atribuicao nao compativel para " + variavel + "\n";
     }
 
-    public static void erroIncompatibilidadeParametros(String funcao, int line) {
-        if(!modificado) modificado = true;
-        text.append("Linha "+line+": incompatibilidade de parametros na chamada de "+funcao).append('\n');
+    public static void erroIncompatibilidadeParametros(String funcao, int linha) {
+        mensagens_de_erro += "Linha " + linha + ": incompatibilidade de parametros na chamada de " + funcao + "\n";
     }
 
     public static String getText() {
-        return text.toString();
-    }
-
-    public static boolean isModificado() {
-        return modificado;
+        return mensagens_de_erro;
     }
 }
